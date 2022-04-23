28f8ec6514c3e1bf99b7250e1ac7e986f6909e15
Alivies <62126694+Alivies@users.noreply.github.com>
2022-04-23 13:58:14 +0700

    update style
    0.1

--- Pdf.cs
+++ Pdf.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 
 namespace lab2
-{
-
+{   
     public class Pdf : Doc
     {
-
         private string Pages { get; set; }
-
         public Pdf(string Name, string Creator, string Tags, string Theme, string Path, string pages) : base(Name, Creator, Tags, Theme, Path)
+        
         {
-
             Pages = pages;
         }
 
         public override void GetInfo()
         {
-
             base.GetInfo();
             Console.WriteLine($"Amount of pages: {Pages}");
         }
