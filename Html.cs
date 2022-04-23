15e3fde7cf9f12b4128761edd7fadb3f10e73f71
Alivies <62126694+Alivies@users.noreply.github.com>
2022-04-23 13:53:30 +0700

    update
    0.1

--- Html.cs
+++ Html.cs
@@ -2,21 +2,15 @@
 
 namespace lab2
 {
-
     public class Html : Doc
     {
-
         private string Link { get; set; }
-
         public Html(string Name, string Creator, string Tags, string Theme, string Path, string link) : base(Name, Creator, Tags, Theme, Path)
         {
-
             Link = link;
         }
-
         public override void GetInfo()
         {
-
             base.GetInfo();
             Console.WriteLine($"Link: {Link}");
         }
