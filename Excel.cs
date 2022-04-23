d37f61d9992d872e977f9c70e7bbfce7dc5ca788
Alivies <62126694+Alivies@users.noreply.github.com>
2022-04-23 13:57:13 +0700

    update
    0.1

--- Excel.cs
+++ Excel.cs
@@ -1,24 +1,21 @@
-﻿using System;
+using System;
 
 namespace lab2
 {
 
     public class Excel : Doc
     {
-
         private string Columns { get; set; }
         private string Rows { set; get; }
-
         public Excel(string Name, string Creator, string Tags, string Theme, string Path, string columns, string rows) : base(Name, Creator, Tags, Theme, Path)
+        
         {
-
             Columns = columns;
             Rows = rows;
         }
 
         public override void GetInfo()
         {
-
             base.GetInfo();
             Console.WriteLine($"Amount of columns: {Columns}");
             Console.WriteLine($"Amount of rows: {Rows}");
